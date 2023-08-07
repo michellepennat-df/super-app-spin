b706bf83d8d630e05f40214fab5914715c4c42f5
Michelle Penna <michelle.penna.ext@digitalfemsa.com>
2023-08-07 14:01:42 -0500

    refactor(): style home

---
+++ src/screens/home/home.style.ts
@@ -0,0 +1,16 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    marginBottom: 24,
+    width: 40,
+    height: 40,
+  },
+});
--- src/screens/home/home.tsx
+++ src/screens/home/home.tsx
@@ -1,5 +1,13 @@
-import {Text} from 'react-native';
+import { Image, View } from 'react-native';
+import MobilePNG from '../../assets/mobile.png';
+import Text from '../../components/Text/Text';
+import { styles } from './home.style';
 
 export const Home = () => {
-  return <Text>Hola soy home</Text>;
+  return (
+    <View style={styles.container}>
+      <Image source={MobilePNG} alt="icon-mobile" style={styles.image} />
+      <Text variant='headline-large'>Pantalla home</Text>
+    </View>
+  );
 };
