823e0a3ba8d288a1ac4222c434b5e868b2a27c3d
Michelle Penna <michelle.penna.ext@digitalfemsa.com>
2023-08-07 12:48:40 -0500

    feat(): create screen home and benefits

---
+++ src/screens/benefits/benefits.tsx
@@ -0,0 +1,5 @@
+import {Text} from 'react-native';
+
+export const benefits = () => {
+  return <Text>Hola soy benefits</Text>;
+};
---
+++ src/screens/home/home.tsx
@@ -0,0 +1,5 @@
+import {Text} from 'react-native';
+
+const home = () => {
+  return <Text>Hola soy home</Text>;
+};
