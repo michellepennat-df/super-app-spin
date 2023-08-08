13aaf59b31f26171846cd7ae77983b1bd6002436
Michelle Penna <michelle.penna.ext@digitalfemsa.com>
2023-08-08 10:18:51 -0500

    format(): change file style

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -1,16 +1,27 @@
 import {Image, View} from 'react-native';
 import Text from '../../components/Text/Text';
-import {styles} from './Home.Style';
+// import {styles} from './Home.Style';
 
 export const Home = () => {
   return (
-    <View style={styles.container}>
-      <Image
-        source={require('../../assets/mobile.png')}
-        alt="icon-mobile"
-        style={styles.image}
-      />
+    <View>
+      <Image source={require('../../assets/mobile.png')} alt="icon-mobile" />
       <Text variant="headline-large">Pantalla home</Text>
     </View>
   );
 };
+
+// style={styles.container}
+// style={styles.image}
+// import {StyleSheet} from 'react-native';
+
+// export const styles = StyleSheet.create({
+//   container: {
+//     width: '100%',
+//     height: '100%',
+//   },
+//   image: {
+//     width: 120,
+//     height: 120,
+//   },
+// });
