7662eed2c8642781ca205fb1f6386fce56b1cf5b
Michelle Penna <michelle.penna.ext@digitalfemsa.com>
2023-08-08 12:37:46 -0500

    refactor(): add images

--- src/screens/benefits/Benefits.tsx
+++ src/screens/benefits/Benefits.tsx
@@ -1,8 +1,10 @@
-import { Image, View } from 'react-native';
+import {Image, ScrollView, View} from 'react-native';
 import Card from '../../components/Card/Card';
 import GridView from '../../components/GridView/GridView';
 import Text from '../../components/Text/Text';
 import PointsTag from '../../components/atoms/Tag/PointsTag';
+import useTheme from '../../hooks/useTheme';
+import {styles} from './Benefits.Style';
 
 const columnOne = (
   <View>
@@ -16,7 +18,11 @@ const columnOne = (
 );
 const columnTwo = (
   <View>
-    <Image source={require('../../assets/images/spin-premia.png')} />
+    <Image
+      style={styles.image}
+      source={require('../../assets/images/spin-premia.png')}
+      alt="Spin Premia"
+    />
   </View>
 );
 
@@ -33,22 +39,49 @@ cards.push(card1);
 cards.push(card2);
 
 export const Benefits = () => {
+  const {colors} = useTheme();
   return (
-    <View>
-      <GridView data={columns} />
-      <GridView data={cards} />
+    <ScrollView
+      style={[styles.container, {backgroundColor: colors.surface_primary}]}>
+      <View>
+        <View>
+          <Text variant="small-body-bold">Tienes</Text>
+          <Text variant="headline-extra-large">10,657</Text>
+          <PointsTag
+            leftIcon={require('../../assets/points.png')}
+            text="Valen $156.00"
+          />
+        </View>
+        <View>
+          <Image
+            style={styles.image}
+            source={require('../../assets/images/spin-premia.png')}
+            alt="Spin Premia"
+          />
+        </View>
+      </View>
+      <View>
+        <Card variant="content-stacked" title="Consulta tu historial" />
+        <Card variant="content-stacked" title="Cambia tus puntos" />
+      </View>
       <Text variant="headline-large">Acumula productos</Text>
       <Text variant="default-body">
         Muy pronto podrás sumar tus compras y ganar productos de regalo
       </Text>
+      <Image source={require('../../assets/images/seals.png')} alt="Sellos" />
       <Text variant="headline-large">Gana más puntos</Text>
       <Text variant="default-body">
         Muy pronto podrás ganar más puntos en el total de tu compra
       </Text>
+      <Image
+        source={require('../../assets/images/aditionals-points.png')}
+        alt="Sellos"
+      />
       <Text variant="headline-large">Suma al comprar</Text>
       <Text variant="default-body">
         Muy pronto podrás acumular compras y llevarte productos de regalo
       </Text>
-    </View>
+      <Image source={require('../../assets/images/rewards.png')} alt="Sellos" />
+    </ScrollView>
   );
 };
