5f03d47d7088bbd4eff7e40b859a76230b28293f
Michelle Penna <michelle.penna.ext@digitalfemsa.com>
2023-08-08 11:13:35 -0500

    format(Movements)

--- src/screens/movements/MovementsTab.tsx
+++ src/screens/movements/MovementsTab.tsx
@@ -1,41 +1,46 @@
-import React, { useState } from 'react'
-import { Animated, View, useWindowDimensions } from 'react-native';
-import { styles } from './Movement.Style';
-import useTheme from '../../hooks/useTheme';
-import TabBar from '../../components/TabBar/TabBar';
+import React, { useState } from 'react';
+import { View, useWindowDimensions } from 'react-native';
 import { SceneMap, TabView } from 'react-native-tab-view';
+import TabBar from '../../components/TabBar/TabBar';
+import useTheme from '../../hooks/useTheme';
+import { styles } from './Movement.Style';
 import AllMovementsList from './allmovements/AllMovementsList';
 
 const Movements = () => {
-    const layout = useWindowDimensions();
-    const { colors } = useTheme()
+  const layout = useWindowDimensions();
+  const {colors} = useTheme();
 
-    const [index, setIndex] = useState(0);
-    const [routes] = useState([
-        { key: 'all', title: 'Todos' },
-        { key: 'earned', title: 'Ganados' },
-        { key: 'used', title: 'Usados' },
-    ]);
-    //TODO: Views for earned and used
-    const renderScene = SceneMap({
-        all: AllMovementsList,
-        earned: AllMovementsList,
-        used: AllMovementsList,
-    });
+  const [index, setIndex] = useState(0);
+  const [routes] = useState([
+    {key: 'all', title: 'Todos'},
+    {key: 'earned', title: 'Ganados'},
+    {key: 'used', title: 'Usados'},
+  ]);
+  //TODO: Views for earned and used
+  const renderScene = SceneMap({
+    all: AllMovementsList,
+    earned: AllMovementsList,
+    used: AllMovementsList,
+  });
 
-
-    return (
-        <View style={[styles.container, { backgroundColor: colors.surface_primary }]}>
-            <TabView
-                navigationState={{ index, routes }}
-                renderScene={renderScene}
-                onIndexChange={setIndex}
-                //TODO: Check for a better Styling
-                renderTabBar={(props) => <TabBar {...props} indicatorContainerStyle={{ marginHorizontal: 10 }} indicatorStyle={{ width: 100 }} />}
-                initialLayout={{ width: layout.width }}
-            />
-        </View>
-    );
-}
+  return (
+    <View style={[styles.container, {backgroundColor: colors.surface_primary}]}>
+      <TabView
+        navigationState={{index, routes}}
+        renderScene={renderScene}
+        onIndexChange={setIndex}
+        //TODO: Check for a better Styling
+        renderTabBar={props => (
+          <TabBar
+            {...props}
+            indicatorContainerStyle={{marginHorizontal: 10}}
+            indicatorStyle={{width: 100}}
+          />
+        )}
+        initialLayout={{width: layout.width}}
+      />
+    </View>
+  );
+};
 
 export default Movements;
--- src/screens/movements/allmovements/AllMovementsList.tsx
+++ src/screens/movements/allmovements/AllMovementsList.tsx
@@ -1,42 +1,44 @@
-import React, { useEffect, useState } from 'react'
-import { ActivityIndicator, SectionList, View } from 'react-native';
-import { style } from './allmovements.style';
-import axios, { AxiosResponse } from 'axios';
-import Text from '../../../components/Text/Text';
+import React from 'react';
+import { SectionList, View } from 'react-native';
+// import { style } from './allmovements.style';
 import ListItem from '../../../components/DataDisplay/ListItem';
-import useMovements from '../../../hooks/useMovements';
+import Text from '../../../components/Text/Text';
 import Spinner from '../../../components/atoms/Spinner/Spinner';
+import useMovements from '../../../hooks/useMovements';
 
 const AllMovementsList = () => {
-
-    const { movements, getMovements, loading, moreData } = useMovements()
-
-    return (
-        <View style={style.container}>
-            <SectionList
-                renderItem={({ item }) => <ListItem
-                    itemName={item.entity}
-                    supportText={item.date}
-                    infoLabel={`+ ${item.points}`}
-                    icon={require('../../../assets/partner_logo.png')}
-                    onPress={() => console.log('')} />}
-                sections={movements}
-                stickySectionHeadersEnabled={false}
-                renderSectionHeader={({ section: { title } }) => (
-                    <Text variant='small-body-bold' style={{ margin: 12 }}>{title}</Text>
-                )}
-                onEndReached={() => moreData ? getMovements() : null}
-                ListFooterComponent={() => {
-                    return (
-                        loading && <Spinner
-                            testID="button-activity-indicator"
-                            size='large'
-                        />
-                    )
-                }}
-            />
-        </View>
-    );
-}
+  const {movements, getMovements, loading, moreData} = useMovements();
+  // style={style.container}
+  return (
+    <View>
+      <SectionList
+        renderItem={({item}) => (
+          <ListItem
+            itemName={item.entity}
+            supportText={item.date}
+            infoLabel={`+ ${item.points}`}
+            icon={require('../../../assets/partner_logo.png')}
+            onPress={() => console.log('')}
+          />
+        )}
+        sections={movements}
+        stickySectionHeadersEnabled={false}
+        renderSectionHeader={({section: {title}}) => (
+          <Text variant="small-body-bold" style={{margin: 12}}>
+            {title}
+          </Text>
+        )}
+        onEndReached={() => (moreData ? getMovements() : null)}
+        ListFooterComponent={() => {
+          return (
+            loading && (
+              <Spinner testID="button-activity-indicator" size="large" />
+            )
+          );
+        }}
+      />
+    </View>
+  );
+};
 
 export default AllMovementsList;
