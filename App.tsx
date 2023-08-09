141c87e6f5586631dad539640f390c81904d8b37
Michelle Penna <michelle.penna.ext@digitalfemsa.com>
2023-08-09 10:12:27 -0500

    delete comments

--- App.tsx
+++ App.tsx
@@ -11,9 +11,6 @@ const App = () => {
       <SafeAreaView style={styles.container}>
         <NavigationContainer>
           <MainNavBar />
-          {/* <NavBar /> */}
-          {/* <TabBar navigationState={{ index: 1, routes }}  layout={{width:100, height:100}} position={interpolation} jumpTo={(key) => console.log(key)}/> */}
-          {/* <Button text="Hola ironhackers" onPress={() => console.log('spin')} /> */}
         </NavigationContainer>
       </SafeAreaView>
     </ThemeProvider>
